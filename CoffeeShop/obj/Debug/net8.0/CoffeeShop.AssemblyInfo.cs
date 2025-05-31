72587329f7c2a6b1326f19cdef9dc3eaa891d693
NgocThi04 <ttnthic720@gmail.com>
2025-05-31 15:12:40 +0700

    section4

--- CoffeeShop/obj/Debug/net8.0/CoffeeShop.AssemblyInfo.cs
+++ CoffeeShop/obj/Debug/net8.0/CoffeeShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c420b4f1bde8ceb58aa09c9fda4cc49721fa6d73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0799c977ced3006567f6597c0dac03572abf04a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
